0c6f6274dee44f1170413650e7b08d710645d3d7
Alexei Baboulevitch <archagon@gmail.com>
2013-11-15 00:29:20 -0600

    Updated profiling macro with num times before print.

--- App Pauser/ABProfiling.h
+++ App Pauser/ABProfiling.h
@@ -15,15 +15,15 @@
                                     static unsigned int totalTimes##x = 0;                                              \
                                     double startTime##x = CACurrentMediaTime();                                         \
 
-#define ABTIME_AVG_END(x, print)    double endTime##x = CACurrentMediaTime();                                           \
-                                    if (totalTimes##x == 0)                                                             \
-                                    {                                                                                   \
-                                        totalTime##x = endTime##x - startTime##x;                                       \
-                                    }                                                                                   \
-                                    else                                                                                \
-                                    {                                                                                   \
-                                        totalTime##x += (endTime##x - startTime##x) / (totalTimes##x + 1);              \
-                                        totalTime##x *= (totalTimes##x + 1) / ((NSTimeInterval)totalTimes##x + 2);      \
-                                    }                                                                                   \
-                                    totalTimes##x++;                                                                    \
-                                    (print ? NSLog(@"Average time for %s: %f", #x, totalTime##x) : 0);
+#define ABTIME_AVG_END(x, printEvery, print)     double endTime##x = CACurrentMediaTime();                                          \
+                                                if (totalTimes##x == 0)                                                             \
+                                                {                                                                                   \
+                                                    totalTime##x = endTime##x - startTime##x;                                       \
+                                                }                                                                                   \
+                                                else                                                                                \
+                                                {                                                                                   \
+                                                    totalTime##x += (endTime##x - startTime##x) / (totalTimes##x + 1);              \
+                                                    totalTime##x *= (totalTimes##x + 1) / ((NSTimeInterval)totalTimes##x + 2);      \
+                                                }                                                                                   \
+                                                totalTimes##x++;                                                                    \
+                                                (print && !(totalTimes##x % printEvery) ? NSLog(@"Average time for %s: %f", #x, totalTime##x) : 0);
